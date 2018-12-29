0b53260ed227c278146c2121acd72950eda10db8
Kaeland Chatman <kaeland1@gmail.com>
2018-12-29 18:04:31 -0500

    added vehicle association to the user model may need for adjustments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :riders, class_name: "User", foreign_key: "driver_id"
   belongs_to :driver, class_name: "User"
   belongs_to :destination
+  belongs_to :vehicle 
 end
