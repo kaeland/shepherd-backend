0f5fd508591c4d26df7db431b0878e249e0fb779
Kaeland Chatman <kaeland1@gmail.com>
2018-12-30 04:40:51 -0500

    removed users routes, and added resource routes for drivers and riders.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :destinations
-  resources :users 
+  resources :drivers
+  resources :riders 
   resources :vehicles
 end
