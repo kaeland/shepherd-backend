3f04f416f65bbd65bedd572313ca2d44e8ae3276
Kaeland Chatman <kaeland1@gmail.com>
2018-12-30 02:17:05 -0500

    migration rollback

--- db/migrate/20181229222717_create_users.rb
+++ db/migrate/20181229222717_create_users.rb
@@ -3,9 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.references :driver, foreign_key: true
-      t.references :destination, foreign_key: true
-      t.references :vehicle, foreign_key: true
+      t.references :driver, foreign_key: true, null: true
+      t.references :destination, foreign_key: true, null: true
+      t.references :vehicle, foreign_key: true, null: true 
     end
   end
 end
