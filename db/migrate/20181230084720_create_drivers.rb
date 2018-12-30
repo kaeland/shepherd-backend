b665cc13ff7b76331e9c822e1494c10c3c70f5db
Kaeland Chatman <kaeland1@gmail.com>
2018-12-30 03:47:48 -0500

    added create_drivers migration file

---
+++ db/migrate/20181230084720_create_drivers.rb
@@ -0,0 +1,9 @@
+class CreateDrivers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :drivers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.references :destination, foreign_key: true
+    end
+  end
+end
